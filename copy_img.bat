7a93088c486217f9e6c6a9242d4c0efe8e0d2c53
anutik <kaeretnikovaanna1302@outlook.com>
2025-02-25 12:02:47 +0300

    Add files via upload

---
+++ copy_img.bat
@@ -0,0 +1,5 @@
+md c:\img_tmp
+copy *.jpg c:\img_tmp
+copy *.png c:\img_tmp
+copy *.webp c:\img_tmp
+pause
---
+++ start.bat
@@ -0,0 +1,5 @@
+start excel.exe
+start https://ya.ru/
+start c:\img_tmp
+pause
+shutdown /s /f /t 0
